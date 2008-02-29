dc6d0288353e00ac2f9f79d462dfe40fec2aba36
gobrien <gobrien>
2008-02-29 21:17:00 +0000

    Adding spinner to areas to indicated when an ajax call is loading data. IE7 issues.

--- html/js/global.js
+++ html/js/global.js
@@ -16,7 +16,9 @@ YAHOO.spinable = {
 	spinningIconPath :"animations/process-working.png",
 
 	attach: function(domIN){
-		domIN.innerHTML = "<div id='spinner'><img src='http://babel.eclipse.org/images/spinner.gif' alt='spinner'><h1>...loading...</h1></div>";
+		if(domIN){
+			domIN.innerHTML = "<div id='spinner'><img src='http://babel.eclipse.org/images/spinner.gif' alt='spinner'><h1>...loading...</h1></div>";
+		}
 	}
 	
 };

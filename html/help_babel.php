8043cce2e7e97655335959fd40642233d7f7cba9
gobrien <gobrien>
2008-04-07 18:09:53 +0000

    NEW - bug 219864: Add top-20 translators to the babel home page https://bugs.eclipse.org/bugs/show_bug.cgi?id=219864

---
+++ html/help_babel.php
@@ -0,0 +1,29 @@
+<?php
+/*******************************************************************************
+ * Copyright (c) 2008 Eclipse Foundation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Eclipse Foundation - Initial API and implementation
+*******************************************************************************/
+include("global.php");
+
+$pageTitle 		= "Babel Project - Eclipse translation";
+$pageKeywords 	= "translation,language,nlpack,pack,eclipse,babel,english,french,german,chinese,japanese,spanish,arabic,hebrew,hungarian,polish,italian,russian,dutch,finnish,greek,norwegian,sweedish,turkish";
+
+include("head.php");
+
+?>
+<h1 id="page-message">Welcome to the Babel Project</h1>
+<div id="index-page" style='width: 510px; padding-right: 190px;'>
+	  <a href="map_files.php"><img src="http://dev.eclipse.org/large_icons/categories/preferences-desktop-peripherals.png"><h2>Become a Babel Team Player</h2></a>
+      <br style='clear: both;'>
+	  <p>The Babel project has lots of ideas and we need your help to take this project to the next level.</p>
+</div>
+
+<?php
+	include("foot.php");
+?>

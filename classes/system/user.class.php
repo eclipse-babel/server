e5dbd10073ed7964fa405fa4783bdfa7874da55a
droy <droy>
2008-07-30 17:54:18 +0000

    [242011] mysql ENCRYPT not available on windows - hinders development

--- classes/system/user.class.php
+++ classes/system/user.class.php
@@ -1,6 +1,6 @@
 <?php
 /*******************************************************************************
- * Copyright (c) 2007 Eclipse Foundation and others.
+ * Copyright (c) 2007-2008 Eclipse Foundation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,6 +9,7 @@
  * Contributors:
  *    Paul Colton (Aptana)- initial API and implementation
  *    Eclipse Foundation 
+ *    Matthew Mazaika <mmazaik  us.ibm.com> - bug 242011
 *******************************************************************************/
 
 class User {
@@ -36,34 +37,50 @@ class User {
 				$email 		= $App->sqlSanitize($email, $dbh);
 				$password 	= $App->sqlSanitize($password, $dbh);
 	
-				$sql = "SELECT *
-					FROM 
-						users 
-					WHERE email = '$email' 
-						AND password_hash = ENCRYPT('$password', password_hash)";
-				$result = mysql_query($sql, $dbh);
-				if($result && mysql_num_rows($result) > 0) {
-					$rValue = true;
-					$myrow = mysql_fetch_assoc($result);
-					
-					$this->userid              = $myrow['userid'];
-					$this->username            = $myrow['username'];
-					$this->first_name          = $myrow['first_name'];
-					$this->last_name           = $myrow['last_name'];
-					$this->email               = $myrow['email'];
-					$this->primary_language_id = $myrow['primary_language_id'];
-					$this->is_committer			= $myrow['is_committer'];
-					$this->hours_per_week      = $myrow['hours_per_week'];
-					$this->updated_on          = $myrow['updated_on'];
-					$this->updated_at          = $myrow['updated_at'];
-					$this->created_on          = $myrow['created_on'];
-					$this->created_at			= $myrow['created_at'];
-
-				}
-				else {
-					$GLOBALS['g_ERRSTRS'][1] = mysql_error();
-				}
+				// since MySQL ENCRYPT is not supported on windows we have to move encryption
+				// from the database layer out to the application layer
+				//  https://bugs.eclipse.org/bugs/show_bug.cgi?id=242011
+				 
+				$hash_query = "SELECT users.password_hash FROM users WHERE email = '$email'";
+				$hash_result = mysql_query($hash_query, $dbh);
 				
+				if ($hash_result && mysql_num_rows($hash_result) > 0) {
+					
+					$hash_row = mysql_fetch_assoc($hash_result);
+					$hash = $hash_row['password_hash'];
+					
+					$sql = "SELECT *
+						FROM 
+							users 
+						WHERE email = '$email' 
+							AND password_hash = '" . crypt($password, $hash) . "'";
+							
+					$result = mysql_query($sql, $dbh);
+					if($result && mysql_num_rows($result) > 0) {
+						$rValue = true;
+						$myrow = mysql_fetch_assoc($result);
+						
+						$this->userid				= $myrow['userid'];
+						$this->username				= $myrow['username'];
+						$this->first_name			= $myrow['first_name'];
+						$this->last_name			= $myrow['last_name'];
+						$this->email				= $myrow['email'];
+						$this->primary_language_id	= $myrow['primary_language_id'];
+						$this->is_committer			= $myrow['is_committer'];
+						$this->hours_per_week		= $myrow['hours_per_week'];
+						$this->updated_on			= $myrow['updated_on'];
+						$this->updated_at			= $myrow['updated_at'];
+						$this->created_on			= $myrow['created_on'];
+						$this->created_at			= $myrow['created_at'];
+	
+					} else {
+						// password failed
+						$GLOBALS['g_ERRSTRS'][1] = mysql_error();
+					}
+				} else {
+					// username failed
+					$GLOBALS['g_ERRSTRS'][1] = mysql_error();
+				}			
 			}
 		}
 		

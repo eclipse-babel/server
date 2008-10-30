2bbc1fff20a85637802c536e4da19051bd0f2252
droy <droy>
2008-10-30 20:30:25 +0000

    245500 Please add a language for estonian

--- babel-setup.sql
+++ babel-setup.sql
@@ -418,6 +418,9 @@ insert into languages set iso_code = 'hi', name = "Hindi";
 insert into languages set iso_code = 'ro', name = "Romanian";
 insert into languages set iso_code = 'uk', name = "Ukrainian";
 insert into languages set iso_code = 'es_CA', name = "Spanish-Catalonian";
+insert into languages values (null, "et_EE", "Estonia", "Estonian", 1);
+
+
 insert into project_versions set project_id = "eclipse", version = "3.4", is_active = 1;
 
 insert into project_versions set project_id = "eclipse", version = "3.3.1", is_active = 0;

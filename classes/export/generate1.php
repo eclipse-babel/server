25c7381c8fa61d35738c8911e37561826deb0fe0
atoulme <atoulme>
2009-02-06 06:44:03 +0000

    [256430] Fragments with no host jeopardize Eclipse installation

--- classes/export/generate1.php
+++ classes/export/generate1.php
@@ -417,7 +417,7 @@ while (($train_row = mysql_fetch_assoc($train_result)) != null) {
 	   METADATA_GENERATOR_LOCATION . " ${output_dir_for_train} ");
 	system("xsltproc -o ${output_dir_for_train}content.xml ".
            dirname(__FILE__) . "/content.xsl ${output_dir_for_train}content.xml");
-    system("cd ${output_dir_for_train} ; jar -fc content.jar artifacts.xml content.xml");
+    system("cd ${output_dir_for_train} ; jar -fc content.jar artifacts.xml content.xml ; rm site.xml");
 }
 echo "Completed generating update site\n";
 

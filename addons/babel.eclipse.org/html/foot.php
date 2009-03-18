0bef82f856629bea488f65611f33da5028cd11db
kitlo <kitlo>
2009-03-18 21:04:24 +0000

    [254432] Provide bugzilla link to report problems with en-US or translations

--- addons/babel.eclipse.org/html/foot.php
+++ addons/babel.eclipse.org/html/foot.php
@@ -9,10 +9,15 @@
  * Contributors:
  *    Paul Colton (Aptana)- initial API and implementation
  *    Eclipse Foundation
+ *    254432 - Provide bugzilla link to report problems with en-US or translations
 *******************************************************************************/
 
 ?>
-<br /><center><a target="_blank" href="https://bugs.eclipse.org/bugs/enter_bug.cgi?product=Babel&component=Server&bug_file_loc=<?= $_SERVER['SCRIPT_NAME'] ?>">Report errors or enhancements</a> | <a href="https://dev.eclipse.org/mailman/listinfo/babel-translators">Discuss translations on the translators mailing list</a></center><br />
+<br /><center>
+	<a target="_blank" href="https://bugs.eclipse.org/bugs/enter_bug.cgi?product=Babel&component=Server&bug_file_loc=<?= $_SERVER['SCRIPT_NAME'] ?>">Report errors or enhancements for server</a>&nbsp;|&nbsp;
+	<a target="_blank" href="https://bugs.eclipse.org/bugs/enter_bug.cgi?product=Babel&component=EnglishStrings&bug_file_loc=URL_TO_THE_STRING&short_desc=Problem%20with%20KEY_NAME&comment=elaborate_here">Report errors for English strings</a>&nbsp;|&nbsp;
+	<a href="https://dev.eclipse.org/mailman/listinfo/babel-translators">Discuss translations on translators mailing list</a>
+</center><br />
 </div></div>
 </div><div id="footer">
 <ul id="footernav">

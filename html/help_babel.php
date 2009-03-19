979fa563a8ea9285b04f247343669aff10265715
kitlo <kitlo>
2009-03-19 01:51:39 +0000

    [258804] Incorrect information

--- html/help_babel.php
+++ html/help_babel.php
@@ -19,6 +19,11 @@ $addon->callHook("head");
 
 ?>
 
+<!--  	
+	Note: the style attribute for the ol tag below was causing a truncation in IE.
+	I removed the attribute. The layout is still acceptable. Future modifications to this file should be tested in IE also.
+-->
+
 <h1 id="page-message">Welcome to the Babel Project</h1>
 <div id="index-page" style='width: 510px; padding-right: 190px;'>
 
@@ -30,7 +35,7 @@ $addon->callHook("head");
 	</p>
 
 	<h3 style='margin-left: 40px;'>Everybody can:</h3>
-	<ol style='margin-left: 20px; margin-top: 0px;'>
+	<ol>
 		<li><a href="translate.php">Help translate Eclipse projects into your language</li>
 		<li><a href="https://bugs.eclipse.org/bugs/enter_bug.cgi?assigned_to=babel.core-inbox%40eclipse.org&blocked=&bug_file_loc=http%3A%2F%2F&bug_severity=normal&bug_status=NEW&comment=&component=Website&contenttypeentry=&contenttypemethod=autodetect&contenttypeselection=text%2Fplain&data=&dependson=&description=&flag_type-1=X&flag_type-2=X&flag_type-4=X&flag_type-6=X&form_name=enter_bug&keywords=&maketemplate=Remember%20values%20as%20bookmarkable%20template&op_sys=All&priority=P3&product=Babel&qa_contact=&rep_platform=All&short_desc=&version=unspecified">Open a bug you found in Babel</a></li>
 		<li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&classification=Technology&product=Babel&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=anywords&keywords=helpwanted+bugday&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&known_name=babel&query_based_on=babel&field0-0-0=product&type0-0-0=substring&value0-0-0=babel&field0-0-1=component&type0-0-1=substring&value0-0-1=babel&field0-0-2=short_desc&type0-0-2=substring&value0-0-2=babel&field0-0-3=status_whiteboard&type0-0-3=substring&value0-0-3=babel&field0-0-4=longdesc&type0-0-4=substring&value0-0-4=babel">Squash a bug already open</a></li>
@@ -38,7 +43,7 @@ $addon->callHook("head");
 	</ol>	  
 
 	<h3 style='margin-left: 40px;'>Projects Members can:</h3>
-	<ol style='margin-left: 20px; margin-top: 0px;'>
+	<ol>
 		<li><a href="importing.php">Get your Eclipse project imported to Babel</a></li>
 		<li>Help make specific language packs for Eclipse projects (pending)</li>
 		<li>Review translations for quality/consistency (pending)</li>

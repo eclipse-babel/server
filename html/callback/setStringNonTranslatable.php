1071f9aeddd33748759356a43eaa047dc12a7975
gobrien <gobrien>
2009-07-28 18:52:18 +0000

    NEW - bug 242904: mark as non-translatable only marks one string https://bugs.eclipse.org/bugs/show_bug.cgi?id=242904

--- html/callback/setStringNonTranslatable.php
+++ html/callback/setStringNonTranslatable.php
@@ -4,28 +4,48 @@ require_once("cb_global.php");
 $string_id = getHTTPParameter("string_id", "POST");
 $checked_state = getHTTPParameter("check", "POST");
 
-$query = "select value from strings where string_id = '".addslashes($string_id)."'";
+$query = "select
+			strings.value, 
+			strings.name as string_name, 
+			files.name as file_name 
+		  from
+		  	strings,
+		  	files
+		  where
+		  	files.file_id = strings.file_id
+		  AND
+		  	strings.string_id = '".addslashes($string_id)."
+		 '";
+
 $res = mysql_query($query,$dbh);
 $row = mysql_fetch_assoc($res);
 
 if($checked_state == "true"){
-	$message = "String '".$row['value']."' has been marked as non-translatable.";
 	$checked_state = 1;
 }else{
-	$message = "String '".$row['value']."' has been marked as translatable.";
 	$checked_state = 0;
 }
 
 $query = "update 
-			strings
+			strings,files
 		  set
-			non_translatable = '".addslashes($checked_state)."' 
+			strings.non_translatable = '".addslashes($checked_state)."' 
 		  where 		  	
-			string_id = '".addslashes($string_id)."'
+			files.name = '".addslashes($row['file_name'])."'
+			AND strings.name = '".addslashes($row['string_name'])."'
+			AND strings.file_id = files.file_id
 		  ";
 
 $res = mysql_query($query,$dbh);
+$updated_rows = mysql_affected_rows();
 
+if($updated_rows < 0){
+	$message = "An error has occurred in processing your request, please file a bug.";
+}elseif ($checked_state == 1) {
+	$message = "'".$row['value']."' has been marked as non-translatable in ".$updated_rows." file(s).";
+} else {
+	$message = "'".$row['value']."' has been marked as translatable in ".$updated_rows." file(s).";
+}
 
 print "<br><br><br><center><b>$message</b></center>";
 ?>

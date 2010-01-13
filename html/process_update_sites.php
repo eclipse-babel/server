0da4aca62f9c39c69066e7d027b2ab927ec55c4e
kitlo <kitlo>
2010-01-13 05:08:26 +0000

    [299402] Extract properties files from Eclipse project update sites for translation

--- html/process_update_sites.php
+++ html/process_update_sites.php
@@ -14,13 +14,10 @@
  * Extract properties files from update sites 
  */
 # To-do: hard-coding the Eclipse, BIRT, & Webtools update sites for now; need to create an UI for project committers to enter their update sites
-$eclipse = array("/home/data/httpd/download.eclipse.org/eclipse/updates/3.6milestones/S-3.6M4-200912101301", "eclipse", "3.6");
-$birt = array("/home/data/httpd/download.eclipse.org/birt/update-site/2.6-interim", "birt", "2.6.0");
-$webtools = array("/home/data/httpd/download.eclipse.org/webtools/updates", "webtools", "3.2");
-
-# BIRT 2.6.0 & Webtools 3.2 are not defined yet, cannot test on staging server
-#$update_sites = array($eclipse, $birt, $webtools);
-$update_sites = array($eclipse);
+$eclipse = array("http://download.eclipse.org/eclipse/updates/3.6milestones/S-3.6M4-200912101301", "eclipse", "3.6");
+$birt = array("http://download.eclipse.org/birt/update-site/2.6-interim", "birt", "2.6.0");
+$webtools = array("http://download.eclipse.org/webtools/updates", "webtools", "3.2");
+$update_sites = array($eclipse, $birt, $webtools);
 
 $temp_dir = "/tmp/tmp-babel/";
 $debug = TRUE;
@@ -39,10 +36,19 @@ if (!isset($User)) {
 require(dirname(__FILE__) . "/../classes/file/file.class.php");
 global $dbh;
 
-if (is_dir($temp_dir)) {
-  exec("rm -rf $temp_dir");
+$temp_unzips_dir = $temp_dir . "unzips/";
+if (is_dir($temp_unzips_dir)) {
+  exec("rm -rf $temp_unzips_dir");
 }
-mkdir($temp_dir) || die("***ERROR: Cannot create working directory $temp_dir\n");
+mkdir($temp_unzips_dir, 0777, TRUE) || die("***ERROR: Cannot create working directory: $temp_unzips_dir\n");
+
+global $addon;
+$context = $addon->callHook('context');
+if ($context == "live") {
+  $rsync_host = "download.eclipse.org::eclipseMirror/";
+} else {
+  $rsync_host = "rsync.osuosl.org::eclipse/";
+}
 
 $files = array();
 $sql = "SELECT * FROM files WHERE is_active = 1";
@@ -58,11 +64,28 @@ while ($myrow_files = mysql_fetch_assoc($rs_files)) {
 }
 
 foreach ($update_sites as $update_site) {
-  $plugins_dir = $update_site[0] . "/plugins/";
+  $site_url = $update_site[0];
   $project_id = $update_site[1];
   $version = $update_site[2];
+  # Sample dirs:
+  # $site_url         http://download.eclipse.org/eclipse/updates/3.6milestones/S-3.6M4-200912101301
+  # $site_dir         eclipse/updates/3.6milestones/S-3.6M4-200912101301/
+  # $site_plugins_dir eclipse/updates/3.6milestones/S-3.6M4-200912101301/plugins/
+  # $temp_site_dir    /tmp/tmp-babel/update_sites/eclipse/3.6/
+  # $temp_unzip_dir   /tmp/tmp-babel/unzips/eclipse/3.6/
+  $site_dir = substr($site_url, strpos($site_url, "/", 7) + 1) . "/";
+  $site_plugins_dir = $site_dir . "/plugins/";
+  $temp_site_dir = $temp_dir . "update_sites/" . $project_id . "/" . $version . "/";
+  $temp_unzip_dir = $temp_dir . "unzips/" . $project_id . "/" . $version . "/";
 
-  chdir($plugins_dir);
+  # Rsync update site
+  exec("mkdir -p $temp_site_dir; rsync -av --delete $rsync_host$site_plugins_dir $temp_site_dir");
+
+  # Make unzip dir
+  mkdir($temp_unzip_dir, 0777, TRUE);
+
+  # Unzip properties files in each jar
+  chdir($temp_site_dir);
   foreach (glob("*.jar") as $jar_name) {
     # Sample jar name: org.eclipse.ui.workbench_3.6.0.I20100105-1530.jar
     #
@@ -74,18 +97,18 @@ foreach ($update_sites as $update_site) {
     $temp_str = substr($temp_str, 0, strrpos($temp_str, "_"));
     $plugin_id = $temp_str;
 
-    mkdir($temp_dir . $project_id);
-    mkdir($temp_dir . $project_id . "/" . $version);
-    mkdir($temp_dir . $project_id . "/" . $version . "/" . $plugin_id);
-    chdir($temp_dir . $project_id . "/" . $version . "/" . $plugin_id);
-    exec("unzip -o -q " . $plugins_dir . $jar_name . " *.properties");
+    mkdir($temp_unzip_dir . $plugin_id, 0777, TRUE);
+    chdir($temp_unzip_dir . $plugin_id);
+    exec("unzip -o -q $temp_site_dir$jar_name *.properties");
   }
 
+  # Collect properties file names
   $properties_file_names = array();
-  chdir($temp_dir . $project_id . "/" . $version);
+  chdir($temp_unzip_dir);
   exec("find . -name *.properties", $properties_file_names);
   sort($properties_file_names);
 
+  # Parse each properties file
   echo "Start processing properties files in project $project_id version $version...\n";
   foreach ($properties_file_names as $properties_file_name) {
     $properties_file_name = substr($properties_file_name, 2);
@@ -107,7 +130,7 @@ foreach ($update_sites as $update_site) {
     if (!$file->save()) {
       echo "***ERROR: Cannot save file $file->name\n";
     } else {
-      $file_name = $temp_dir . $project_id . "/" . $version . "/" . $properties_file_name;
+      $file_name = $temp_unzip_dir . $properties_file_name;
       $file->parseProperties(file_get_contents($file_name));
       if ($debug) {
         echo "  " . $file->name . "\n";
